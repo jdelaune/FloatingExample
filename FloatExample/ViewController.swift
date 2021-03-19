19976fa55f33c91484b7972423222390039787b6
Jordan de Laune <jordan@chargifi.com>
2021-03-19 12:43:56 +0000

    Initial Commit

---
+++ FloatExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloatExample
+//
+//  Created by Jordan de Laune on 19/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
